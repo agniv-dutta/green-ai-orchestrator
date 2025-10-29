994b1f526af5ba3822e300f93287131fa38421a0
AdityaC-07 <aditya.choudhuri@somaiya.edu>
2025-10-30 05:12:00 +0530

    Delete ml-service directory

--- ml-service/services/__init__.py
+++
@@ -1,9 +0,0 @@
-from .carbon_service import CarbonIntensityService
-from .optimization_engine import GreenAIOptimizationEngine
-from .data_processor import DataProcessor
-
-__all__ = [
-    'CarbonIntensityService',
-    'GreenAIOptimizationEngine', 
-    'DataProcessor'
-]
--- ml-service/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .logger import setup_logging
-from .metrics import MetricsCalculator
-
-__all__ = [
-    'setup_logging',
-    'MetricsCalculator'
-]
